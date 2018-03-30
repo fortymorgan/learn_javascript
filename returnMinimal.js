6d16baa9570d6321a0659a9a8070337af986f3c1
Forty Morgan <dimazhmudikov@gmail.com>
2018-03-30 15:23:34 +0300

    Good visual in code

--- returnMinimal.js
+++ returnMinimal.js
@@ -5,4 +5,5 @@ function returnMinimal(a, b) {
 var a = prompt("Введите число a:", "");
 var b = prompt("Введите число b:", "");
 
-(a == b) ? alert("Числа равны") : alert("Меньшее число: " + returnMinimal(a, b));
+(a == b) ? alert("Числа равны") :
+alert("Меньшее число: " + returnMinimal(a, b));
